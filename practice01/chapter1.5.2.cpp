eeac4239130489d4eaa4f1e4390c57ba3e315000
KongWiKi <kongwiki@163.com>
2018-06-10 00:06:33 +0800

    添加该章练习题

---
+++ practice01/chapter1.5.2.cpp
@@ -0,0 +1,18 @@
+//
+// Created by kongweikun on 18-6-9.
+//
+//数据类型与输入格式
+//
+#include <stdio.h>
+#include <tgmath.h>
+
+int main(){
+    printf("%f\n",11111.0*11111);
+    printf("%d\n",11111*11111);
+//    溢出
+//    printf("%d",111111111*111111111);
+    printf("%f\n",0.0/0.0);
+    printf("%f\n",1.0/0.0);
+    printf("%d",1/0);
+
+}
