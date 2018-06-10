c89764a3ba32a5ad6c48f6633a44f191124ae168
KongWiKi <kongwiki@163.com>
2018-06-10 23:06:38 +0800

    更新

--- practice01/chapter1.5.2.cpp
+++ practice01/chapter1.5.2.cpp
@@ -7,12 +7,16 @@
 #include <tgmath.h>
 
 int main(){
-    printf("%f\n",11111.0*11111);
-    printf("%d\n",11111*11111);
+//    printf("%f\n",11111.0*11111);
+//    printf("%d\n",11111*11111);
 //    溢出
 //    printf("%d",111111111*111111111);
-    printf("%f\n",0.0/0.0);
-    printf("%f\n",1.0/0.0);
-    printf("%d",1/0);
-
+//    printf("%f\n",0.0/0.0);
+//    printf("%f\n",1.0/0.0);
+//    printf("%d",1/0);
+    printf("%ld", sizeof(int));
+    printf("%ld", sizeof(double));
+    double i=1.0,j=3.0;
+    i=i/j;
+    printf("%f",i);
 }
