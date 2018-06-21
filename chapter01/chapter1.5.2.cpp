9c0f7087f0f97dbcd688ec37271d591095b759bb
KongWiKi <kongwiki@163.com>
2018-06-21 09:00:31 +0800

    测试

--- chapter01/chapter1.5.2.cpp
+++ chapter01/chapter1.5.2.cpp
@@ -21,6 +21,14 @@ int main(){
 //    printf("%f",i);
 //    printf("%d",533%100);
 //    printf("%d",534%10);
-    printf("%d\n",543/10%10);
-    printf("%d",543%10);
+//    printf("%d\n",543/10%10);
+//    printf("%d\n",543%10);
+//    printf("%d\n",41%3);
+    int a = 2;
+    printf("%d\n",a++ +3 );
+    printf("%d\n",a);
+//    for (int i =0;i + 1< 4;i++){
+//        printf("%d",i);
+//    }
+
 }
