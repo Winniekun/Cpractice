a45fc52715a313d299fc4ca26814bdd8a6757e62
KongWiKi <kongwiki@163.com>
2018-06-23 00:10:24 +0800

    添加指针学习

---
+++ base/pointer/chapter09.1.cpp
@@ -0,0 +1,13 @@
+//
+// Created by kongweikun on 18-6-23.
+//
+#include <stdio.h>
+
+int main(){
+    int a = 0, b = 1;
+    char c = 'A';
+    printf("a is %d, &a is %p\n",a,&a);
+    printf("b is %d, &b is %p\n",b,&b);
+    printf("c is %d, &c is %p\n",c,&c);
+    return 0;
+}
