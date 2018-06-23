f16166435e296c2149455fc875fb899c6dcc10f7
KongWiKi <kongwiki@163.com>
2018-06-23 12:44:39 +0800

    添加指针练习

--- base/pointer/chapter09.2.cpp
+++ base/pointer/chapter09.2.cpp
@@ -1,4 +1,18 @@
 //
 // Created by kongweikun on 18-6-23.
 //
-
+//指针变量的定义和初始化
+#include <stdio.h>
+int main(){
+    int a = 0, b = 1;
+    char c = 'A';
+    int *pa = NULL, *pb = NULL;//对其进行初始化
+    char *pc = NULL;//对其进行初始化
+    pa = &a;
+    pb = &b;
+    pc = &c;
+    printf("a is %d, &a is %p, pa is %p\n",a, &a, pa);
+    printf("b is %d, &b is %p, pb is %p\n",b, &b, pb);
+    printf("c is %d, &c is %p, pc is %p\n",c, &c, pc);
+    return 0;
+}
--- base/pointer/chapter09.3.cpp
+++ base/pointer/chapter09.3.cpp
@@ -1,4 +1,18 @@
 //
 // Created by kongweikun on 18-6-23.
 //
+//指针运算符
+#include <stdio.h>
+int main(){
+    int a = 0, b = 1;
+    char c = 'A';
+    int *pa = &a;
+    int *pb = &b;
+    char *pc = &c;
+    *pa = 9;
+    printf("a is %d, &a is %p, pa is %p *pa is %d\n",a, &a, pa, *pa);
+    printf("b is %d, &b is %p, pb is %p *pb is %d\n",b, &b, pb, *pb);
+    printf("c is %d, &c is %p, pc is %p *pc is %c\n",c, &c, pc, *pc);
+
+}
 
