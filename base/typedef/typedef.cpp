dd446a400d94089738fec403ef207fc5d2be44a1
KongWiKi <kongwiki@163.com>
2018-08-07 12:11:31 +0800

    添加结构体的学习:neutral_face::weary:

--- base/typedef/typedef.cpp
+++ base/typedef/typedef.cpp
@@ -6,26 +6,27 @@
 #include <stdio.h>
 #include <string.h>
 
-#define NAME_LEN  64 /*姓名的字符数*/
+typedef struct  date{
+    int year;
+    int month;
+    int day;
+} DATE;
 
-/*==表示学生的结构体==*/
-struct student{
-    char name[NAME_LEN];
-    int height;
-    float weight;
-    long schols;
-};
+typedef struct student{
+    long studentID;
+    char studentName[10];
+    char studentSex;
+    DATE birthday;
+    int score[4];
+} STUDENT;
 
-int main(void){
-    struct student sanaka;
-    strcpy(sanaka.name, "Sanaka");
-    sanaka.height = 175;
-    sanaka.weight = 62.5;
-    sanaka.schols = 73000;
-    
-    printf("姓名 = %s\n", sanaka.name);
-    printf("身高 = %d\n", sanaka.height);
-    printf("体重 = %f\n", sanaka.weight);
-    printf("奖学金 = %ld\n", sanaka.schols);
-    
+int main(){
+    STUDENT stu1 = {100310121, "孔维坤", 'M', {1996,10,22}, {89, 83, 99,100}};
+    STUDENT stu2;
+    stu2 = stu1;
+    printf("stu2: %ld %8s%3c%6d/%02d/%02d%4d%4d%4d%4d\n",stu2.studentID,stu2.studentName,
+    stu2.studentSex, stu2.birthday.year,stu2.birthday.month,stu2.birthday.day,
+    stu2.score[0],stu2.score[1],stu2.score[2],stu2.score[3]);
+    return 0;
 }
+
--- base/typedef/typedefPointer.cpp
+++ base/typedef/typedefPointer.cpp
@@ -2,3 +2,15 @@
 // Created by kongweikun on 18-8-7.
 //
 
+//结构体指针的定义和初始化
+typedef struct student{
+    long studentID;
+    char studentName[10];
+    char studentSex;
+    int score[4];
+} STUDENT;
+
+STUDENT *p; /*定义一个指向STUDRNT结构体的指针变量*/
+
+
+
